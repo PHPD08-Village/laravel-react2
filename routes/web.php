27d3ae1e50b8866c7907de8f7d7aa393cc0f6d82
PHPD08-Village <fakesugarvillage@gmail.com>
2024-12-28 00:42:46 +0800

    更新 web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PubForStarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublishController;
@@ -9,6 +8,7 @@ use App\Http\Controllers\UserInfoController;
 use App\Http\Controllers\ErrorLogController; // 添加新的控制器
 use App\Http\Controllers\SearchController; // 添加新的控制器
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\PubForStarController;
 use App\Http\Controllers\PubForHomeCaseController;
 use App\Http\Controllers\FreelancerForHomeController;
 use App\Http\Controllers\PubForCaseMngController;
@@ -28,6 +28,31 @@ Route::post('/api/userinfo', [UserInfoController::class, 'store']);
 // 案件搜尋頁頁面
 Route::get('/api/get-userinfo-publish', [SearchController::class, 'getdata']);
 
+// 阿桂的記得放前面
+Route::get('/api/projects', [ProjectController::class, 'index']); // 取得所有作品
+Route::get('/api/projects/{id}', [ProjectController::class, 'show']); // 取得單一作品
+Route::post('/api/projects', [ProjectController::class, 'store']); // 新增作品
+Route::put('/api/projects/{id}', [ProjectController::class, 'update']); // 更新作品
+Route::delete('/api/projects/{id}', [ProjectController::class, 'destroy']); // 刪除作品
+
+// 戶長的
+// Route::post('/api/star', [StarController::class, 'store']);
+// Route::get('/api/get-star', [StarController::class, 'getAllstar']);
+// Route::get('/api/star/{uid}', [StarController::class, 'getUserInfo']);
+// Route::get('/api/case/{caseId}', [PubForStarController::class, 'getCaseInfo']);
+// Route::get('/api/get-latest-projectsUser', [PubForHomeLatestController::class, 'getLatestProjUser']);   // 新增首頁最新案件發案人資訊 API
+// Route::get('/api/get-latest-projects', [PubForHomeCaseController::class, 'getLatestProjects']);   // 新增首頁最新案件案件資訊 API
+// Route::get('/api/get-clickhighest-projects', [PubForHomeCaseController::class, 'getCliHighestProjects']);   // 新增首頁點閱率最高案件資訊 API
+// Route::get('/api/get-starhighest-taker', [FreelancerForHomeController::class, 'getStarHighestTaker']);   // 新增首頁點閱率最高案件資訊 API
+
+// 案件管理(暫時把登入條件拿掉)
+// Route::middleware('auth')->group(function () {
+    // Route::get('/api/get-cases/{userId}', [PubForCaseMngController::class, 'getCases']);
+    // Route::post('/api/get-cases/{cid}/switch-case', [PubForCaseMngController::class, 'toggle']);
+    // });
+
+
+
 // 登入登出功能
 // Route::middleware(['auth'])->group(function () {
 //     // 使用者資訊表單
@@ -37,24 +62,6 @@ Route::get('/api/get-userinfo-publish', [SearchController::class, 'getdata']);
 
 // 添加錯誤日誌的路由
 Route::post('/api/log-error', [ErrorLogController::class, 'logError']);
-
-
-// React 路由的配置應該在後面
-// 戶長的
-Route::post('/api/star', [StarController::class, 'store']);
-// Route::get('/api/get-star', [StarController::class, 'getAllstar']);
-// Route::get('/api/star/{uid}', [StarController::class, 'getUserInfo']);
-Route::get('/api/case/{caseId}', [PubForStarController::class, 'getCaseInfo']);
-// Route::get('/api/get-latest-projectsUser', [PubForHomeLatestController::class, 'getLatestProjUser']);   // 新增首頁最新案件發案人資訊 API
-Route::get('/api/get-latest-projects', [PubForHomeCaseController::class, 'getLatestProjects']);   // 新增首頁最新案件案件資訊 API
-Route::get('/api/get-clickhighest-projects', [PubForHomeCaseController::class, 'getCliHighestProjects']);   // 新增首頁點閱率最高案件資訊 API
-Route::get('/api/get-starhighest-taker', [FreelancerForHomeController::class, 'getStarHighestTaker']);   // 新增首頁點閱率最高案件資訊 API
-
-// 案件管理(暫時把登入條件拿掉)
-// Route::middleware('auth')->group(function () {
-Route::get('/api/get-cases/{userId}', [PubForCaseMngController::class, 'getCases']);
-Route::post('/api/get-cases/{cid}/switch-case', [PubForCaseMngController::class, 'toggle']);
-// });
 
 // React 路由的配置應該在後面
 Route::get('/', function () {
@@ -70,10 +77,4 @@ Route::get('/{any}', function(){
 // Route::post('/api/submit-data', [DataController::class, 'mydata']);
 
 
-// 阿桂的
-Route::get('/api/projects', [ProjectController::class, 'index']); // 取得所有作品
-Route::get('/api/projects/{id}', [ProjectController::class, 'show']); // 取得單一作品
-Route::post('/api/projects', [ProjectController::class, 'store']); // 新增作品
-Route::put('/api/projects/{id}', [ProjectController::class, 'update']); // 更新作品
-Route::delete('/api/projects/{id}', [ProjectController::class, 'destroy']); // 刪除作品
 
