af74868675943ba83f1984cdd7b8d156676e00bc
kuu304 <kuuei234@gmail.com>
2025-01-06 11:18:15 +0800

    修改api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserInfoController;
 use App\Http\Controllers\PersonalDataController;
 
+Route::get('/userinfo', [UserInfoController::class, 'index']);
+Route::post('/userinfo', [UserInfoController::class, 'store']);
 
 Route::post('/store-user', [PersonalDataController::class, 'store']); 
