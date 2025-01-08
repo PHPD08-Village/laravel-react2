fd5472fa3fca161ca00aef313787e2f73fe5cf93
kuu304 <kuuei234@gmail.com>
2025-01-08 11:31:10 +0800

    解決衝突

--- database/migrations/2024_12_18_233608_update_publish_table.php
+++ database/migrations/2024_12_18_233608_update_publish_table.php
@@ -6,22 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 class UpdatePublishTable extends Migration
 {
-    public function up(): void
-    {
-        Schema::table('publish', function (Blueprint $table) {
-            $table->string('title')->after('uid'); // 添加新欄位 title
-            $table->string('contact_name')->after('title'); // 添加新欄位 contact_name
-            $table->timestamp('completion_time')->after('contact_name'); // 添加完成時間欄位
-            $table->decimal('budget', 10, 2)->after('completion_time'); // 添加預算欄位
-            $table->string('location')->after('budget'); // 添加地點欄位
-            $table->string('phone')->after('location'); // 添加新欄位 phone
-            $table->string('email')->after('phone'); // 添加新欄位 email
-            $table->text('details')->after('email'); // 添加新欄位 details
-            $table->string('require_code')->nullable()->after('details'); // 添加新欄位 require_code
-            $table->dropColumn('project_name'); // 刪除 project_name 欄位
-            $table->dropColumn('description'); // 刪除 description 欄位
-        });
-    }
+    // public function up(): void
+    // {
+        // Schema::table('publish', function (Blueprint $table) {
+        //     $table->string('title')->after('uid'); // 添加新欄位 title
+        //     $table->string('contact_name')->after('title'); // 添加新欄位 contact_name
+        //     $table->timestamp('completion_time')->after('contact_name'); // 添加完成時間欄位
+        //     $table->decimal('budget', 10, 2)->after('completion_time'); // 添加預算欄位
+        //     $table->string('location')->after('budget'); // 添加地點欄位
+        //     $table->string('phone')->after('location'); // 添加新欄位 phone
+        //     $table->string('email')->after('phone'); // 添加新欄位 email
+        //     $table->text('details')->after('email'); // 添加新欄位 details
+        //     $table->string('require_code')->nullable()->after('details'); // 添加新欄位 require_code
+        //     $table->dropColumn('project_name'); // 刪除 project_name 欄位
+        //     $table->dropColumn('description'); // 刪除 description 欄位
+    //     });
+    // }
 
     public function down(): void
     {
