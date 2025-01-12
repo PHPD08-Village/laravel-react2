48a15b5c8c16ca3126e68f985f02c3e6c74855a3
kuu304 <kuuei234@gmail.com>
2025-01-12 16:14:48 +0800

    加上預設照片

--- resources/js/components/Personal_editor2/UserProfile.jsx
+++ resources/js/components/Personal_editor2/UserProfile.jsx
@@ -17,12 +17,12 @@ const UserProfile = () => {
                     <input type="file" id="avatarInput" accept="image/*" hidden />
                     <label htmlFor="avatarInput" className="avatar-box">
                         {/* 目前無法顯示照片我滿頭問號 */}
-                        <img src={userData.headshot} alt="大頭貼" className="avatar-img" /> 
+                        <img src={userData.headshot || "./imgs/dphoto.jpg"} alt="大頭貼" className="avatar-img" /> 
                     </label>
                 </div>
                 <div className="avatar-details">
                     <span>你好！{userData.username}</span>
-                    <span>會員編號：{userData.uid}</span>
+                    <span>會員編號：000{userData.uid}</span>
                 </div>
             </div>
             <button className="top-bar-button"><Link to="/publish" className='alink'>刊登新服務</Link></button>
