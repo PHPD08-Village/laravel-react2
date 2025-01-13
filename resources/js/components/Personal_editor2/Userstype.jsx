d9397190d57146d2968fe4e98965ee22d3015b0f
simon-ke <a40515041@gmail.com>
2025-01-13 14:30:51 +0800

    更新邏輯

--- resources/js/components/Personal_editor2/Userstype.jsx
+++ resources/js/components/Personal_editor2/Userstype.jsx
@@ -5,9 +5,9 @@ import { useUser } from './UserProvider';
 const Userstype = () => {
     const { userRole, setUserRole, section, setSection } = useUser();
 
-    const handleRoleChange = (userRole) => { // 儲存點擊 接案者 or 業主
+    const handleRoleChange = (userRole, section) => { // 儲存點擊 接案者 or 業主
         setUserRole(userRole);
-        setSection('');
+        setSection(section);
     };
 
     const handleSectionChange = (section) => {
@@ -19,9 +19,9 @@ const Userstype = () => {
             {/* <!-- 切換按鈕 --> */}
             <div className="switcher">
                 {/* 業主 */}
-                <button className={`switch-btn ${userRole === '業主' ? 'active' : ''}`} onClick={() => handleRoleChange('業主')}>業主</button>                
+                <button className={`switch-btn ${userRole === '業主' ? 'active' : ''}`} onClick={() => handleRoleChange('業主', '接案資料')}>業主</button>                
                 {/* 接案者 */}
-                <button className={`switch-btn ${userRole === '接案者' ? 'active' : ''}`} onClick={() => handleRoleChange('接案者')}>接案者</button>
+                <button className={`switch-btn ${userRole === '接案者' ? 'active' : ''}`} onClick={() => handleRoleChange('接案者', '接案資料')}>接案者</button>
             </div>
             {/* 透過上面的按鈕來判斷顯示的內容 */}
             {userRole === '接案者' ? (
@@ -59,6 +59,7 @@ const Userstype = () => {
                         <hr />
                         <ul>
                             <li className={`sidebar-item ${section === '編輯個人資料' ? 'active' : ''}`} onClick={() => handleSectionChange('編輯個人資料')}>編輯個人資料</li>
+                            <li id="case-info" className={`sidebar-item ${section === '接案資料' ? 'active' : ''}`} onClick={() => handleSectionChange('接案資料')}>接案資料</li>
                         </ul>
                     </div>
                     <div className="menu-item">
